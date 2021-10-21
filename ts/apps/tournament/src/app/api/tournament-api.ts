1ab697bbed41a8aea979f39dbf1f54281e9edead
Maxime <maxime.warluzel@ynov.com>
2021-10-21 17:55:33 +0200

    Add ENDPOINTS 1,2 and 3 (1/2)

--- ts/apps/tournament/src/app/api/tournament-api.ts
+++ ts/apps/tournament/src/app/api/tournament-api.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { TournamentRepository } from '../repository/tournament-repository';
 import { TournamentToAdd } from './api-model';
+import { Participant } from './api-model';
 import { v4 as uuidv4 } from 'uuid';
 
 const tournamentRepository = new TournamentRepository();
@@ -11,8 +12,13 @@ export const postTournament = (req: Request, res: Response) => {
   const tournament = { id: uuidv4(), name: tournamentToAdd.name, phases: [], participants: [] };
   tournamentRepository.saveTournament(tournament);
 
+  if (tournament.name == null) {
+    res.status(400);
+    res.send("❌ Le champ 'nom' est manquant ou vide ! ❌");
+  }
+  
   res.status(201);
-  res.send({ id: tournament.id });
+  res.send({ id: tournament.id, name: tournament.name });
 };
 
 export const getTournament = (req: Request, res: Response) => {
@@ -22,4 +28,9 @@ export const getTournament = (req: Request, res: Response) => {
 
   res.status(200);
   res.send(tournament);
+};
+
+export const addParticipant = (req: Request, res: Response) => {
+  const playerToAdd: Participant = req.body;
+
 };
--- ts/apps/tournament/src/testing/tournament.spec.ts
+++ ts/apps/tournament/src/testing/tournament.spec.ts
@@ -1,10 +1,15 @@
 import { app } from '../app';
 import * as request from 'supertest';
-import { Tournament, TournamentPhaseType } from '../app/api/api-model';
+import { Participant, Tournament, TournamentPhaseType } from '../app/api/api-model';
 
 const exampleTournament = {
   name: 'Unreal',
 } as Tournament;
+
+const exampleParticipant = {
+  name: 'Novak Djokovic',
+  elo: 2500
+} as Participant;
 
 describe('/tournament endpoint', () => {
   describe('[POST] when creating a tournament', () => {
@@ -14,12 +19,22 @@ describe('/tournament endpoint', () => {
       expect(body.id).not.toBeUndefined();
     });
 
+    it('should return a name', async () => {
+      await request(app).post('/api/tournaments').send({}).expect(400);
+    });
+
     it('should have stored the tournament', async () => {
       const { body } = await request(app).post('/api/tournaments').send(exampleTournament).expect(201);
 
       const get = await request(app).get(`/api/tournaments/${body.id}`).expect(200);
 
       expect(get.body.name).toEqual(exampleTournament.name);
+    });
+
+    it('should not return the correct name & elo', async () => {
+      const { body } = await request(app).post('/api/tournaments').send(exampleTournament).expect(201);
+
+      await request(app).post(`/api/tournaments/${body.id}/participants`).send(body.id, exampleParticipant).expect(400);
     });
   });
 });
